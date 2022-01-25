20db3122fc50459c229dd2c5271e5b862c09c335
Klimovich Yauheniya <zh.shuravina@gmail.com>
2022-01-25 23:24:15 +0300

    burger btn active js commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,23 @@
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+// console.log(test(5, 2));
+
+// включение burger_menu по клику
+
+function burgerMenu() {
+    let burgerIcon = document.querySelector(".header__burger-btn");
+    let closeIcon = document.getElementById("close_icon");
+    let burgerMenu = document.getElementById("burger_menu");
+
+    burgerIcon.addEventListener("click", function (e) {
+       burgerMenu.classList.toggle("header__navigation_active");
+       this.style.display = "none";
+       closeIcon.style.display = "block";
+    })
+
+    closeIcon.addEventListener("click", function () {
+        burgerMenu.classList.toggle("header__navigation_active");
+        this.style.display = "none";
+        burgerIcon.style.display = "block";
+    })
+}
+
+burgerMenu();
